12eba8fa43c09b52300d932b007140f1c8d49f5d
Dimo <bafmaamu@yahoo.com>
2019-04-30 15:49:02 +0200

    Add Support for Weighted Grading

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -14,11 +14,13 @@ namespace GradeBook.GradeBooks
         public string Name { get; set; }
         public List<Student> Students { get; set; }
         public GradeBookType Type { get; set; }
+        public bool IsWeighted { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
             Students = new List<Student>();
+            IsWeighted = isWeighted;
         }
 
         public void AddStudent(Student student)
@@ -107,20 +109,31 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            var baseGrade = 0;
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    baseGrade = 4;
+                    break;
                 case 'B':
-                    return 3;
+                    baseGrade = 3;
+                    break;
                 case 'C':
-                    return 2;
+                    baseGrade = 2;
+                    break;
                 case 'D':
-                    return 1;
+                    baseGrade = 1;
+                    break;
                 case 'F':
-                    return 0;
+                    baseGrade = 0;
+                    break;
             }
-            return 0;
+
+            var weightedPoint =
+                this.IsWeighted && (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled)
+                    ? 1
+                    : 0;
+            return baseGrade + weightedPoint;
         }
 
         public virtual void CalculateStatistics()
